ceccd701a49ff33b6ffaefb1c1f69a1a3420e751
krishnaadithya <krishnaadithya@gmail.com>
2024-07-28 20:35:33 +0000

    add modified changes

--- src/tools/reload_qwen_vit.py
+++ src/tools/reload_qwen_vit.py
@@ -1,10 +1,13 @@
 import torch
 from transformers import AutoModelForCausalLM
+import os 
 
 torch.manual_seed(1234)
 
 qwen_model_path = 'pretrained/Qwen-VL-Chat'
-save_path = 'pretrained/QwenViT/qwen_vit_G.pt'
+save_folder = 'pretrained/visual_tokenizer/'
+os.makedirs(save_folder, exist_ok = True)
+save_path = os.path.join(save_folder,'qwen_vit_G.pt')
 
 model = AutoModelForCausalLM.from_pretrained(qwen_model_path, device_map="cpu", trust_remote_code=True).eval()
 
